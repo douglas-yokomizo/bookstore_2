22c01dd3982e22aca39488c7b333a945284b093a
cardrocha <cardrocharj@gmail.com>
2023-12-28 09:24:53 -0300

    fix:migrate-auth

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,54 +26,53 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-DEBUG = True
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", '0.0.0.0']
 
 # Application definition
 
 INSTALLED_APPS = [
-  "django.contrib.admin",
-  "django.contrib.auth",
-  "django.contrib.contenttypes",
-  "django.contrib.sessions",
-  "django.contrib.messages",
-  "django.contrib.staticfiles",
-  "django_extensions",
-  "rest_framework",
-  "order",
-  "product",
-  "debug_toolbar",
-  "rest_framework.authtoken",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "rest_framework",
+    "order",
+    "product",
+    "debug_toolbar",
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
-  "django.middleware.security.SecurityMiddleware",
-  "django.contrib.sessions.middleware.SessionMiddleware",
-  "django.middleware.common.CommonMiddleware",
-  "django.middleware.csrf.CsrfViewMiddleware",
-  "django.contrib.auth.middleware.AuthenticationMiddleware",
-  "django.contrib.messages.middleware.MessageMiddleware",
-  "django.middleware.clickjacking.XFrameOptionsMiddleware",
-  "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
 
 TEMPLATES = [
-  {
-      "BACKEND": "django.template.backends.django.DjangoTemplates",
-      "DIRS": [],
-      "APP_DIRS": True,
-      "OPTIONS": {
-          "context_processors": [
-              "django.template.context_processors.debug",
-              "django.template.context_processors.request",
-              "django.contrib.auth.context_processors.auth",
-              "django.contrib.messages.context_processors.messages",
-          ],
-      },
-  },
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
@@ -83,14 +82,14 @@ WSGI_APPLICATION = "bookstore.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  "default": {
-      "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-      "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-      "USER": os.environ.get("SQL_USER", "user"),
-      "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-      "HOST": os.environ.get("SQL_HOST", "localhost"),
-      "PORT": os.environ.get("SQL_PORT", "5432"),
-  }
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
+    }
 }
 
 
@@ -98,18 +97,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-  {
-      "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-  },
-  {
-      "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-  },
-  {
-      "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-  },
-  {
-      "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-  },
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -136,15 +135,15 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = [
-  "127.0.0.1",
+    '127.0.0.1',
 ]
 
-REST_FRAMEWORK = {
-  "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-  "PAGE_SIZE": 5,
-  "DEFAULT_AUTHENTICATION_CLASSES": [
-    "rest_framework.authentication.BasicAuthentication",
-    "rest_framework.authentication.SessionAuthentication",
-    "rest_framework.authentication.TokenAuthentication",
-],
+REST_FRAMEWORK =  {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
