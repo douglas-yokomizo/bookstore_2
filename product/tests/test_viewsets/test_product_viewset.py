0ad0c8bd946dab60275964c2838592adf93a6d03
cardrocha <cardrocharj@gmail.com>
2023-12-30 14:05:16 -0300

    fix: code test_product

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -26,7 +26,7 @@ class TestProductViewSet(APITestCase):
 
     def test_get_all_product(self):
         token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token" + token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -38,10 +38,10 @@ class TestProductViewSet(APITestCase):
 
     def test_create_product(self):
         token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION="Token" + token.key)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         category = CategoryFactory()
         data = json.dumps(
-            {"title": "notebook", "price": 800.00, "categories_id": [category.id]}
+            {"title": "notebook", "price": 800.00, "categories": [category.id]}
         )
 
         response = self.client.post(
